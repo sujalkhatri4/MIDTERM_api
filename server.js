ce162f3d463cee01e01bb401aec4df5744d46b49
Sujal <sujalkhatri411@gmail.com>
2024-10-18 15:58:55 -0400

    connecton created for  database (cluster)

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// MongoDB connection string
+const mongoURI = 'mongodb+srv://veroproduction4:sujalapi@cluster4.rmdge.mongodb.net/';
+// Connection  to MongoDB
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
